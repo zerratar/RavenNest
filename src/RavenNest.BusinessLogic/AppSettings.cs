d4ac81d199b483a39b9b89b26174a4e18664fc96
zerratar <zerratar@gmal.com>
2019-11-04 14:04:37 +0100

    Use blob storage for keys and key vault for protection

--- src/RavenNest.BusinessLogic/AppSettings.cs
+++ src/RavenNest.BusinessLogic/AppSettings.cs
@@ -1,5 +1,11 @@
 ﻿namespace RavenNest.BusinessLogic
 {
+    public class AzureAppSettings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string KeyIdentifier { get; set; }
+    }
     public class AppSettings
     {
         public string DbConnectionString { get; set; }
