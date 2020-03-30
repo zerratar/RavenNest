4d762cfc238d69002fecd5f842ab7489df0dc719
karlj <kj@solidicon.com>
2020-03-30 12:23:10 +0200

    Add island,task,taskargument for playerjoin

--- src/RavenNest.Models/PlayerAdd.cs
+++ src/RavenNest.Models/PlayerAdd.cs
@@ -4,5 +4,8 @@
     {
         public string UserId { get; set; }
         public string UserName { get; set; }
+        public string Island { get; set; }
+        public string Task { get; set; }
+        public string TaskArgument { get; set; }
     }
 }
