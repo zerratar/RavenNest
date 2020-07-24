d525511cbf76c0831ba3ea1a2138d3f9fc3621e0
Maximilian Ast <ast.maximilian@gmail.com>
2020-07-25 01:00:23 +0200

    Added timing middelware

---
+++ src/RavenNest/RequestTiming.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace RavenNest
+{
+    public class RequestTiming
+    {
+        public static async Task TimingMiddleware(HttpContext context, Func<Task> next)
+        {
+            var logger = context.RequestServices.GetService<ILogger<RequestTiming>>();
+            var sw = new Stopwatch();
+            sw.Start();
+            try
+            {
+                await next.Invoke();
+            }
+            finally
+            {
+                sw.Stop();
+
+                if (sw.ElapsedMilliseconds > 1000)
+                    logger.LogWarning($"Very long request finished after {sw.ElapsedMilliseconds}ms ({context.Request.Path})");
+                else
+                    logger.LogInformation($"Request finished after {sw.ElapsedMilliseconds}ms ({context.Request.Path})");
+
+            }
+        }
+    }
+
+    public static class RequestTimingExt
+    {
+        public static void AddRequestTiming(this IApplicationBuilder app)
+        {
+            app.Use(RequestTiming.TimingMiddleware);
+        }
+    }
+}
