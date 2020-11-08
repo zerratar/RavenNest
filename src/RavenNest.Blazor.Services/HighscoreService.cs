eb2f358c63021b2ec8bcc0b5729292fc99cae7a3
zerratar <zerratar@gmail.com>
2020-11-08 12:32:50 +0100

    Added simple highscore page and made sure "dashboard" link gets visible automatically when you login.

---
+++ src/RavenNest.Blazor.Services/HighscoreService.cs
@@ -0,0 +1,26 @@
+﻿using RavenNest.BusinessLogic.Game;
+using RavenNest.Models;
+using System.Threading.Tasks;
+
+namespace RavenNest.Blazor.Services
+{
+    public class HighscoreService
+    {
+        private readonly IHighScoreManager highScoreManager;
+
+        public HighscoreService(IHighScoreManager highScoreManager)
+        {
+            this.highScoreManager = highScoreManager;
+        }
+
+        public HighScoreCollection GetHighscore(int offset, int take)
+        {
+            return highScoreManager.GetHighScore(offset, take);
+        }
+
+        public async Task<HighScoreCollection> GetHighscoreAsync(int offset, int take)
+        {
+            return await Task.Run(() => GetHighscore(offset, take));
+        }
+    }
+}
