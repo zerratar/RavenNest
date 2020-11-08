fce00f0ca12b9a8a5ac14563ff1e68ea0c680670
zerratar <zerratar@gmail.com>
2020-11-08 20:42:50 +0100

    Added simple items page

---
+++ src/RavenNest.Blazor.Services/ItemService.cs
@@ -0,0 +1,29 @@
+﻿using RavenNest.BusinessLogic.Game;
+using RavenNest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RavenNest.Blazor.Services
+{
+    public class ItemService
+    {
+        private readonly IItemManager itemManager;
+        public ItemService(IItemManager itemManager)
+        {
+            this.itemManager = itemManager;
+        }
+
+        public ItemCollection GetItems()
+        {
+            return itemManager.GetAllItems();
+        }
+
+        public async Task<ItemCollection> GetItemsAsync()
+        {
+            return await Task.Run(() => itemManager.GetAllItems());
+        }
+    }
+}
