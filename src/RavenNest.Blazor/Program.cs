a2e52f567c5aeecd7319993506731f362261e4ac
zerratar <zerratar@gmail.com>
2021-04-10 00:20:19 +0200

    bump

--- src/RavenNest.Blazor/Program.cs
+++ src/RavenNest.Blazor/Program.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RavenNest.Blazor
 {
