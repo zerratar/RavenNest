0dd39fc815824a02593b87c311f5b0e5b365035a
Karl <zerratar@gmail.com>
2021-06-05 09:18:31 +0200

    Add Market Item Transaction Log
    This is for tracking purchases on the marketplace for later being able to show some statistics on how players evaluate their items

---
+++ src/RavenNest.DataModels/MarketItemTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RavenNest.DataModels
+{
+    public class MarketItemTransaction : Entity<MarketItemTransaction>
+    {
+        private Guid id; public Guid Id { get => id; set => Set(ref id, value); }
+        private Guid itemId; public Guid ItemId { get => itemId; set => Set(ref itemId, value); }
+        private long amount; public long Amount { get => amount; set => Set(ref amount, value); }
+        private decimal pricePerItem; public decimal PricePerItem { get => pricePerItem; set => Set(ref pricePerItem, value); }
+        private decimal totalPrice; public decimal TotalPrice { get => totalPrice; set => Set(ref totalPrice, value); }
+        private Guid buyerCharacterId; public Guid BuyerCharacterId { get => buyerCharacterId; set => Set(ref buyerCharacterId, value); }
+        private Guid sellerCharacterId; public Guid SellerCharacterId { get => sellerCharacterId; set => Set(ref sellerCharacterId, value); }
+        private DateTime created; public DateTime Created { get => created; set => Set(ref created, value); }
+    }
+}
