ae7af7bb548173a0c4929f1f003eea3b132d1c06
Karl <zerratar@gmail.com>
2021-09-01 14:28:04 +0200

    Fix an issue where user properties was not stored properly

--- src/RavenNest.DataModels/UserProperty.cs
+++ src/RavenNest.DataModels/UserProperty.cs
@@ -7,7 +7,7 @@ namespace RavenNest.DataModels
         private Guid id; public Guid Id { get => id; set => Set(ref id, value); }
         private Guid userId; public Guid UserId { get => userId; set => Set(ref userId, value); }
         private string propertyKey; public string PropertyKey { get => propertyKey; set => Set(ref propertyKey, value); }
-        private string value; public string Value { get => value; set => Set(ref value, value); }
+        private string value; public string Value { get => value; set => Set(ref this.value, value); }
         private DateTime? updated; public DateTime? Updated { get => updated; set => Set(ref updated, value); }
         private DateTime created; public DateTime Created { get => created; set => Set(ref created, value); }
     }
