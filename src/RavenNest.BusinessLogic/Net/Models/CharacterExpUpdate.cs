63777ceed1fef12ad63c5b469c5243cedbcefcd2
Karl <zerratar@gmail.com>
2021-10-24 14:51:44 +0200

    Allow individual skills to be updated from game client
    This will open up for closer to realtime updates on the server of the currently trained stats.

---
+++ src/RavenNest.BusinessLogic/Net/Models/CharacterExpUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RavenNest.BusinessLogic.Net
+{
+    public class CharacterExpUpdate
+    {
+        public Guid CharacterId { get; set; }
+        public int SkillIndex { get; set; }
+        public int Level { get; set; }
+        public double Experience { get; set; }
+    }
+
+}
--- src/RavenNest.BusinessLogic/Net/PacketHandlers/UpdateCharacterSkillPacketHandler.cs
+++ src/RavenNest.BusinessLogic/Net/PacketHandlers/UpdateCharacterSkillPacketHandler.cs
@@ -36,4 +36,36 @@ namespace RavenNest.BusinessLogic.Net
             await connection.ReplyAsync(packet.CorrelationId, packet.Type, result, CancellationToken.None);
         }
     }
+
+    internal class UpdateCharacterExperiencePacketHandler : IGamePacketHandler
+    {
+        private readonly ILogger logger;
+        private readonly IPlayerManager playerManager;
+
+        public UpdateCharacterExperiencePacketHandler(ILogger logger, IPlayerManager playerManager)
+        {
+            this.logger = logger;
+            this.playerManager = playerManager;
+        }
+        public async Task HandleAsync(IWebSocketConnection connection, GamePacket packet)
+        {
+            var result = false;
+
+            if (packet.TryGetValue<CharacterExpUpdate>(out var update))
+            {
+                result = this.playerManager.UpdateExperience(
+                    connection.SessionToken,
+                    update.SkillIndex,
+                    update.Level,
+                    update.Experience,
+                    update.CharacterId);
+            }
+            else
+            {
+                logger.LogError("CharacterExpUpdate package received but the packet data did not contain the proper structure.");
+            }
+
+            await connection.ReplyAsync(packet.CorrelationId, packet.Type, result, CancellationToken.None);
+        }
+    }
 }
