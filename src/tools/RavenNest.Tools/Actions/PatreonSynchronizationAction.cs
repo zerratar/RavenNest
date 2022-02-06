b62eea60f94a8c7881f48afc7d8893bb65ad4fef
Karl <zerratar@gmail.com>
2022-02-06 19:26:37 +0100

    Add uncommited changes

---
+++ src/tools/RavenNest.Tools/Actions/PatreonSynchronizationAction.cs
@@ -0,0 +1,37 @@
+﻿using RavenNest.PatreonAPI;
+using Shinobytes.Console.Forms;
+
+namespace RavenNest.Tools.Actions
+{
+    public class PatreonSynchronizationAction
+    {
+
+        const string CAMPAIGN_ID = "";
+        const string ACCESS_TOKEN = "";
+
+        public ProgressBar ToolProgress { get; }
+        public TextBlock ToolStatus { get; }
+
+        public PatreonSynchronizationAction(ProgressBar toolProgress, TextBlock toolStatus)
+        {
+            this.ToolProgress = toolProgress;
+            this.ToolStatus = toolStatus;
+            //confirmDialog = new SaveConfirmationWindow();
+        }
+
+        public async void Apply()
+        {
+            var p = new PatreonAPI.PatreonAPI(ACCESS_TOKEN);
+            //var data2 = p.GetMembers();
+            var data = p.fetchCampaignMembers(CAMPAIGN_ID, 100);
+
+
+            var patreon = new PatreonClient(ACCESS_TOKEN);
+            //var pledges = await patreon.GetCampaignPledges(CAMPAIGN_ID);
+
+            var members = await patreon.GetCampaignMembers(CAMPAIGN_ID);
+            //var campaign = await patreon.GetCampaign(CAMPAIGN_ID);
+
+        }
+    }
+}
