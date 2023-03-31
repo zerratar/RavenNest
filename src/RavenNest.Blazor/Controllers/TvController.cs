2467b2dba2c82f045c043f7e28ab0eb0a831fd52
zerratar <zerratar@gmail.com>
2023-03-31 13:55:01 +0200

    Add WIP api endpoint for Ravenfall Tv

---
+++ src/RavenNest.Blazor/Controllers/TvController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RavenNest.BusinessLogic.Data;
+using RavenNest.BusinessLogic.Game;
+using RavenNest.Models.Tv;
+
+namespace RavenNest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TvController : GameApiController
+    {
+        public TvController(
+            ILogger<TvController> logger,
+            GameData gameData,
+            IAuthManager authManager,
+            SessionInfoProvider sessionInfoProvider,
+            SessionManager sessionManager,
+            ISecureHasher secureHasher)
+            : base(logger, gameData, authManager, sessionInfoProvider, sessionManager, secureHasher)
+        {
+        }
+
+        [HttpGet("episode/{episodeId}")]
+        public async Task<EpisodeResult> GetEpisodeAsync(Guid episodeId)
+        {
+            // not implemented yet
+
+            return new EpisodeResult()
+            {
+                Id = episodeId,
+                Status = EpisodeGenerationStatus.NotFound,
+            };
+        }
+
+        [HttpPost("episode")]
+        public async Task<EpisodeResult> GenerateEpisodeAsync([FromBody] GenerateEpisodeRequest request)
+        {
+            // not implemented yet
+
+            return new EpisodeResult()
+            {
+                Id = request.Id,
+                Status = EpisodeGenerationStatus.Error,
+            };
+        }
+    }
+}
---
+++ src/RavenNest.Models/Tv/Episode.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RavenNest.Models.Tv
+{
+    public class GenerateEpisodeRequest
+    {
+        /// <summary>
+        ///     The requested description of the episode, leave empty for have one generated.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        ///     The requested description of the episode, leave empty for have one generated.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        ///     The requested outro/conclusion of the episode, leave empty for have one generated.
+        /// </summary>
+        public string Outro { get; set; }
+
+        /// <summary>
+        ///     The available characters to be in the show, not all characters are guaranteed to be included.
+        /// </summary>
+        public List<Episode.Character> Characters { get; set; }
+
+        /// <summary>
+        ///     This will be the Id of the generated episode.
+        /// </summary>
+        /// 
+        public Guid Id { get; set; }
+
+    }
+
+    public enum EpisodeGenerationStatus
+    {
+        Generating,
+        Completed,
+        Error,
+        NotFound,
+    }
+
+    public class EpisodeResult
+    {
+        public Guid Id { get; set; }
+        public Episode Episode { get; set; }
+        public EpisodeGenerationStatus Status { get; set; }
+    }
+
+    public class Episode
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+        [JsonPropertyName("user_id")]
+        public Guid? UserId { get; set; }
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("outro")]
+        public string Outro { get; set; }
+
+        [JsonPropertyName("characters")]
+        public Character[] Characters { get; set; }
+
+        [JsonPropertyName("dialogues")]
+        public Dialogue[] Dialogues { get; set; }
+
+        public class Character
+        {
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+            [JsonPropertyName("gender")]
+            public string Gender { get; set; }
+            [JsonPropertyName("race")]
+            public string Race { get; set; }
+            [JsonPropertyName("job")]
+            public string Job { get; set; }
+            [JsonPropertyName("description")]
+            public string Description { get; set; }
+
+            [JsonPropertyName("strength")]
+            public int Strength { get; set; }
+
+            public override string ToString()
+            {
+                return "id: " + Id +
+                    ", name: \"" + Name + "\"" +
+                    ", gender: " + Gender +
+                    ", race: " + Race +
+                    ", job: " + Job +
+                    ", strength: " + Strength +
+                    ", description: \"" + Description + "\"";
+            }
+        }
+
+        public class Dialogue
+        {
+            [JsonPropertyName("animation")]
+            public string Animation { get; set; }
+            [JsonPropertyName("character_name")]
+            public string CharacterName { get; set; }
+            [JsonPropertyName("location")]
+            public string Location { get; set; }
+
+            [JsonPropertyName("text")]
+            public string Text { get; set; }
+
+            public override string ToString()
+            {
+                return Animation + ", " + CharacterName + ": \"" + Text + "\"";
+            }
+        }
+    }
+
+}
