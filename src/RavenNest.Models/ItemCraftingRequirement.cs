051952175029d1c81e3e98c662558e55dc91d01c
zerratar <zerratar@gmail.com>
2023-08-09 14:59:29 +0200

    Preparation time for certain crafting things

--- src/RavenNest.Models/ItemCraftingRequirement.cs
+++ src/RavenNest.Models/ItemCraftingRequirement.cs
@@ -23,17 +23,19 @@ namespace RavenNest.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
         public Guid ItemId { get; set; }
         public Guid? FailedItemId { get; set; }
         public double SuccessRate { get; set; }
+        /// <summary>
+        ///     How long in seconds will it take for this to be created (per item)
+        /// </summary>
+        public double PreparationTime { get; set; }
         /// <summary>
         /// Whether or not the success rate is fixed or if it should be calculated based on the players skill level.
         /// </summary>
         public bool FixedSuccessRate { get; set; }
         public int RequiredLevel { get; set; }
         public int RequiredSkill { get; set; }
-
         public List<ItemRecipeIngredient> Ingredients { get; set; }
     }
 
